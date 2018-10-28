8e16037a24ebecbc6efcbf080026795dc71c5c15
mrdotb <mrdotb@protonmail.com>
2018-10-28 23:46:43 +0100

    1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirSocks5.MixProject do
     [
       app: :elixir_socks5,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
