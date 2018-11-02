a5e5a318f93b33eb3133d745df2144b8a68b1a9d
mrdotb <mrdotb@protonmail.com>
2018-11-02 22:47:10 +0100

    Edit tests to match the new methods list format.

--- test/handshake_test.exs
+++ test/handshake_test.exs
@@ -4,12 +4,12 @@ defmodule HanshakeTest do
 
   test "valid handshake 1" do
     packet = <<5, 2, (<<0, 1>>)>>
-    assert %Handshake{ver: 5, nmethods: 2, methods: <<0, 1>>} = Handshake.new(packet)
+    assert %Handshake{ver: 5, nmethods: 2, methods: [0, 1]} = Handshake.new(packet)
   end
 
   test "valid handshake 2" do
     packet = <<5, 3, (<<0, 1, 2>>)>>
-    assert %Handshake{ver: 5, nmethods: 3, methods: <<0, 1, 2>>} = Handshake.new(packet)
+    assert %Handshake{ver: 5, nmethods: 3, methods: [0, 1, 2]} = Handshake.new(packet)
   end
 
   test "invalid handshake version" do
